3dddec2ffd93dd4b7ae58ee6cdee2f505eaea9fb
MandhareAmit <amitmandhare27@gmail.com>
2021-03-04 16:01:42 +0530

    add start script

--- app.js
+++ app.js
@@ -176,6 +176,8 @@ app.use((err, req, res, next) => {
     //res.send('Ohh Sorry, Something Went Wrong');
 })
 
-app.listen(3000, () => {
-    console.log('Running on port 3000');
+const port =  process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
 });
